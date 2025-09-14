50752e7d5f5afa04e54835c2013d2d89895239a6
Michael-Richardson <michael.richardson@gmail.com>
2025-09-14 15:23:01 -0500

    Code from September 14 meeting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -4,34 +4,32 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Shooter {
-    private DcMotor motor0   = null;
-    private DcMotor motor1   = null;
+    private DcMotor shooterMotor0   = null;
+    private DcMotor shooterMotor1   = null;
 
     public Shooter(HardwareMap hardwareMapInit) {
-
-        motor0 = hardwareMapInit.get(DcMotor .class, "motor0");
-        motor1 = hardwareMapInit.get(DcMotor.class, "motor1");
-        motor1.setDirection(DcMotor.Direction.REVERSE);
+        shooterMotor0 = hardwareMapInit.get(DcMotor .class, "motor0");
+        shooterMotor1 = hardwareMapInit.get(DcMotor.class, "motor1");
+        shooterMotor1.setDirection(DcMotor.Direction.REVERSE);
     }
-
 
     public void shootSlow(){
-        motor0.setPower(.5);
-        motor1.setPower(.5);
-    }
-
-    public void shootFast(){
-        motor0.setPower(1);
-        motor1.setPower(1);
+        shooterMotor0.setPower(.5);
+        shooterMotor1.setPower(.5);
     }
 
     public void shootMedium(){
-        motor0.setPower(.75);
-        motor1.setPower(.75);
+        shooterMotor0.setPower(.75);
+        shooterMotor1.setPower(.75);
+    }
+
+    public void shootFast(){
+        shooterMotor0.setPower(1);
+        shooterMotor1.setPower(1);
     }
 
     public void shootStop(){
-        motor0.setPower(0);
-        motor1.setPower(0);
+        shooterMotor0.setPower(0);
+        shooterMotor1.setPower(0);
     }
 }
