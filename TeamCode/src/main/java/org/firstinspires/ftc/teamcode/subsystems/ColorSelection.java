283e73393e08d22783188cc69ed15ae5e1796d13
Michael-Richardson <michael.richardson@gmail.com>
2025-11-09 10:07:36 -0600

    Updates from 11/7/2025 and 11/8/2025 - Updated ColorSelection and Auto code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ColorSelection.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import static com.qualcomm.robotcore.eventloop.opmode.OpMode.blackboard;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class ColorSelection
+{
+    private LinearOpMode myOpMode = null;
+    public static final String ALLIANCE_KEY = "Alliance";
+    public static final String DESIRED_TAG_ID = "Tag ID";
+
+    public ColorSelection(LinearOpMode opMode){
+        myOpMode = opMode;
+
+    }
+    public void selectColor(){
+        myOpMode.telemetry.clearAll();
+
+        while(!myOpMode.isStopRequested()){
+            myOpMode.telemetry.addLine("choose red or blue");
+            myOpMode.telemetry.addLine("push option button for blue");
+            myOpMode.telemetry.addLine("push share button for red");
+            myOpMode.telemetry.addLine("push home/ps to select option");
+            if (myOpMode.gamepad1.options){
+                //blue
+                blackboard.put(DESIRED_TAG_ID, 20);
+                blackboard.put(ALLIANCE_KEY, "BLUE");
+            }
+            if (myOpMode.gamepad1.share) {
+                //red
+                blackboard.put(DESIRED_TAG_ID, 24);
+                blackboard.put(ALLIANCE_KEY, "RED");
+            }
+            if (myOpMode.gamepad1.ps){
+                break;
+            }
+
+           displayColor();
+        }
+
+        myOpMode.telemetry.clearAll();
+    }
+
+    public void displayColor(){
+        if (blackboard.get(ALLIANCE_KEY) == "BLUE"){
+            myOpMode.gamepad1.setLedColor(0,0,255,-1);
+        }
+        if (blackboard.get(ALLIANCE_KEY) == "RED") {
+            myOpMode.gamepad1.setLedColor(255, 0, 0, -1);
+
+        }
+        myOpMode.telemetry.addData("Alliance", blackboard.get(ALLIANCE_KEY));
+        myOpMode.telemetry.addData("Tag Id", blackboard.get(DESIRED_TAG_ID));
+        myOpMode.telemetry.update();
+    }
+
+}
+
